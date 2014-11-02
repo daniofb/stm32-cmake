de29fc3cf4c5f77c19f9b718e47e71dce0ed5e53
Daniel K <daniofb@gmail.com>
2014-11-03 00:41:07 +0100

    Add files

---
+++ cmake/Modules/arm-none-eabi.cmake
@@ -0,0 +1,31 @@
+find_program(ARM-NONE-EABI "arm-none-eabi-gcc")
+if (NOT ARM-NONE-EABI)
+	message( FATAL_ERROR "No cross compiler found")
+
+endif(NOT ARM-NONE-EABI)
+
+INCLUDE(CMakeForceCompiler)
+
+set(CMAKE_SYSTEM_NAME Generic)
+
+set( CROSS_COMPILE arm-none-eabi- )
+
+CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
+CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
+set(CMAKE_ASM_COMPILER arm-none-eabi-as)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+
+# SET(CMAKE_C_FLAGS "-mthumb -mcpu=cortex-m0plus -fno-builtin -Wall -std=gnu99 -fdata-sections -ffunction-sections" CACHE INTERNAL "c compiler flags")
+SET(CMAKE_C_FLAGS "-mthumb -mcpu=cortex-m0plus" CACHE INTERNAL "c compiler flags")
+# SET(CMAKE_CXX_FLAGS "-mthumb -mcpu=cortex-m0plus -fno-builtin -fdata-sections -ffunction-sections" CACHE INTERNAL "cxx compiler flags")
+SET(CMAKE_CXX_FLAGS "-mthumb -mcpu=cortex-m0plus" CACHE INTERNAL "cxx compiler flags")
+SET(CMAKE_ASM_FLAGS "-mthumb -mcpu=cortex-m0plus" CACHE INTERNAL "asm compiler flags")
+
+# SET(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -Wl,-Map=mmap.map,--cref,--no-warn-mismatch -mthumb -mcpu=cortex-m0plus" CACHE INTERNAL "exe link flags")
+set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -mcpu=cortex-m0 -mthumb" CACHE INTERNAL "exe link flags")
+
+set(CMAKE_FIND_ROOT_PATH "/usr/arm-none-eabi")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
---
+++ src/main.c
@@ -0,0 +1,37 @@
+#include "stm32l053xx.h"
+#include "system_stm32l0xx.h"
+
+volatile uint32_t msTicks = 0;                                       /* Variable to store millisecond ticks */
+
+void SysTick_Handler(void)
+{
+    msTicks++;
+}
+
+void _init(void){}
+
+int main(void)
+{
+    SystemInit();
+    
+    SysTick_Config(SystemCoreClock / 1000);      /* Configure SysTick to generate an interrupt every millisecond */
+    
+    RCC->IOPENR |= RCC_IOPENR_GPIOAEN; 
+ 
+    GPIOA->MODER |= (GPIO_MODER_MODE5_0) ;
+    /* Configure PC8 and PC9 in output  mode  */
+
+    GPIOA->MODER &= ~(3 << (2 * 5));
+    GPIOA->MODER |= 1 << (2 * 5);
+    int every_one_sec = msTicks;
+    while (1)
+    {
+        if(msTicks == (1000 + every_one_sec))
+        {
+            GPIOA->ODR ^= 0x20;
+            msTicks = every_one_sec;
+        }
+    }
+ 
+    return 0;
+}
